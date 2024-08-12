e1349c0b7f69ac822f1be4960de1668d5effa39e
Khayam Ijaz <ijazkhayam@gmail.com>
2024-08-12 18:57:59 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ import { MdOutlinePlayCircleOutline } from "react-icons/md";
 const Hero = () => {
   return (
     <>
-      <div className="w-full border-t h-auto min-h-[102vh] bg-[#37517E]">
-      <div className="md:w-[80%] w-[90%] mx-auto h-full grid md:mt-10 mt-16 grid-r grid-cols-12 items-center ">
+      <div className="w-full border-t h-auto min-h-[105vh] bg-[#37517E]">
+      <div className="md:w-[80%] w-[90%] mx-auto h-full grid md:mt-10 mt-20 grid-r grid-cols-12 gap-y-3 items-center ">
         <div className="md:col-span-6 col-span-12 md:order-1 order-2">
             <h1 className='text-white font-bold sm:text-5xl text-3xl'>Better Solutions for Your Business</h1>
             <h1 className='text-gray-300 font-semibold mt-1 text-xl md:text-2xl'>We are team of talented designers making websites with Bootstrap</h1>
